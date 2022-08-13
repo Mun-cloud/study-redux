e8b0e7c93de0cacd4cdf19379bb496012f2e1591
문태호(Mun TaeHo) <mun05170@naver.com>
2022-08-13 22:19:41 +0900

    study redux

---
+++ src/routes/Home.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ToDo from "../components/ToDo";
+import { addToDo } from "../store";
+
+function Home() {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+  const toDos = useSelector((state) => state);
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addToDo(text));
+    setText("");
+  };
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Home;
